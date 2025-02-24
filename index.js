c5dac4da4daaeb04b5c1fdb160f4a641b993d733
Frankincense <frankincensewelsey@gmail.com>
2025-02-24 21:28:44 +0300

    Manipulating arrays test

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+// Solutions to testcases
+function destructivelyAppendCat(name) {
+    return cats.push(name);
+}
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+function destructivelyRemoveLastCat(name) {
+    return cats.pop();
+}
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift();
+}
+function appendCat(name) {
+    const moreCats = [...cats, name];
+    return moreCats;
+}
+function prependCat(name) {
+    const moreCats = [name, ...cats];
+    return moreCats;
+}
+function removeFirstCat() {
+    return cats.slice(1);
+}
+function removeLastCat() {
+    return cats.slice(0, -1);
+}
